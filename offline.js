b006b24b0b7ec0119b0c61e1514ca2c3946321dc
MrARESIII <97580081+MrARESIII@users.noreply.github.com>
2022-09-17 16:12:40 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1663419296,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/air.png",
+		"images/dirt.png",
+		"images/player-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground6.png",
+		"images/tiledbackground7.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
